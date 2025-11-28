0138e71f9ca8eedf0805d29c5b572aca79f5b84d
vincentbensadi <vincent.bensadi@viveris.fr>
2025-11-28 09:36:54 +0100

    Initial Commit

---
+++ src/main/java/epita/projet_bensadi_vincent/ProjetBensadiVincentApplication.java
@@ -0,0 +1,13 @@
+package epita.projet_bensadi_vincent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBensadiVincentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetBensadiVincentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/epita/projet_bensadi_vincent/ProjetBensadiVincentApplicationTests.java
@@ -0,0 +1,13 @@
+package epita.projet_bensadi_vincent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBensadiVincentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
